d83756a619e7c60f122c1000d32a0a3541489f18
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-02-04 15:10:04 +0530

    Created required components commit 2

---
+++ MegaBlogWebApp/src/components/PostCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import storageService from "../appwrite/storageConfig";
+
+function PostCard({ $id, title, feturedImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-100 p-4 rounded-xl">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={storageService.getFilePreview(feturedImage)}
+            alt={title}
+            className="rounded-xl"
+          />
+        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
--- MegaBlogWebApp/src/components/index.js
+++ MegaBlogWebApp/src/components/index.js
@@ -3,5 +3,29 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Login from "./Login";
+import Button from "./Button";
+import RTE from "./RTE";
+import AuthLayout from "./AuthLayout";
+import SignUp from "./SignUp";
+import PostForm from "./post-form/PostForm";
 
-export { Header, Footer, Logo, LogoutBtn, Container };
+export {
+  Header,
+  Footer,
+  Logo,
+  LogoutBtn,
+  Container,
+  Input,
+  PostCard,
+  Select,
+  Login,
+  Button,
+  RTE,
+  AuthLayout,
+  SignUp,
+  PostForm,
+};
