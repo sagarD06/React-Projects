db3f883d8ae9a5a05f488c51b0bc8bc8c723ae53
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-02-08 12:27:24 +0530

    modifications

--- MegaBlogWebApp/src/App.jsx
+++ MegaBlogWebApp/src/App.jsx
@@ -3,8 +3,7 @@ import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Header, Footer } from "./components";
-import { Outlet } from 'react-router-dom'
-import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
